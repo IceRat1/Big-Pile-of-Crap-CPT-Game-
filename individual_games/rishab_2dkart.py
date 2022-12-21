e2ae951d1f6d58d7020ef0b3ad1812c94374bf24
Rishab432 <75451609+Rishab432@users.noreply.github.com>
2022-12-21 18:17:27 -0500

    Update rishab_2dkart.py

--- individual_games/rishab_2dkart.py
+++ individual_games/rishab_2dkart.py
@@ -1,7 +1,9 @@
 import pygame
-from pygame.locals import K_ESCAPE, KEYDOWN, QUIT
+import random
 
 pygame.init()
+pygame.font.init()
+pygame.mixer.init()
 
 WIDTH = 640
 HEIGHT = 480
@@ -18,39 +20,90 @@ class kartgame():
     def __init__(self) -> None:
         self.player1x = 200
         self.player1y = 200
+        self.p1x_velo = 0
+        self.p1y_velo = 0
+
         self.player2x = 400
         self.player2y = 400
+        self.p2x_velo = 0
+        self.p2y_velo = 0
+
         self.running = True
 
-    def event_getter():
+    def event_getter(self) -> None:
         for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    running = False
-            elif event.type == QUIT:
-                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
+            elif event.type == pygame.QUIT:
+                self.running = False
 
-    def run():
-        running = True
-        while running:
-            
+    def movement(self) -> None:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            if self.p1x_velo < 5:
+                self.p1x_velo += 1
+        if keys[pygame.K_s]:
+            pass
+        if keys[pygame.K_a]:
+            pass
+        if keys[pygame.K_d]:
+            pass
 
-            # GAME STATE UPDATES
-            # All game math and comparisons happen here
+    def waluigitrack(self) -> None:
+        self.running = True
+        while self.running:
+            kartgame.event_getter(self)
 
-            # DRAWING
-            screen.fill((255, 255, 255))  # always the first drawing command
+            screen.fill((128, 0, 128))
 
-            
 
-            # Must be the last two lines
-            # of the game loop
             pygame.display.flip()
             clock.tick(30)
-            #---------------------------
+        pygame.quit()
+
+    def meadowstrack(self) -> None:
+        self.running = True
+        while self.running:
+            kartgame.event_getter(self)
+
+            screen.fill((0, 0, 0))
 
 
-    pygame.quit()
+            pygame.display.flip()
+            clock.tick(30)
+        pygame.quit()
+
+    def mariotrack(self) -> None:
+        self.running = True
+        while self.running:
+            kartgame.event_getter(self)
+
+            screen.fill((178, 0, 0))
+
+
+            pygame.display.flip()
+            clock.tick(30)
+        pygame.quit()
+
+    def run(self) -> None:
+        track = random.randrange(0, 3)
+        print(track)
+        if track == 0:
+            pygame.mixer.music.load("Waluigi Pinball - Mario Kart DS - Super Smash Bros. Ultimate.mp3")
+            pygame.mixer.music.set_volume(0.7)
+            pygame.mixer.music.play(-1)
+            kartgame.waluigitrack(self)
+        elif track == 1:
+            pygame.mixer.music.load("Moo Moo Meadows - Mario Kart Wii.mp3")
+            pygame.mixer.music.set_volume(0.7)
+            pygame.mixer.music.play(-1)
+            kartgame.meadowstrack(self)
+        else:
+            pygame.mixer.music.load("Super Mario Kart Music (SNES) - Mario Circuit.mp3")
+            pygame.mixer.music.set_volume(0.7)
+            pygame.mixer.music.play(-1)
+            kartgame.mariotrack(self)
 
 if __name__ == "__main__":
     game = kartgame()
